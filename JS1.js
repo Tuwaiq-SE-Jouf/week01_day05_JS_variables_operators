b3a1abd44fd39d4c3107153d4382e2924d2c49d5
yousef4355 <a1081643882@gmail.com>
2021-10-14 17:33:03 +0300

    hallo

---
+++ JS1.js
@@ -0,0 +1,28 @@
+console.log("hello")
+let firstName="yousef";
+console.log(firstName)
+let num1=7;
+let num2=2;
+console.log("num1="+""+num2+""+"num2="+num1)
+let num3 =num1+num2;
+console.log(num3)
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+
+console.log(num1)
+ lastName=prompt("whats your lastName");
+ console.log(lastName)
+ children=prompt("how mach your childern");
+ wife=prompt("whats name your wife");
+ job=prompt("whats your job");
+ console.log("your children"+children)
+ console.log("nme your wife"+wife)
+ console.log("your job"+job)
+
+raduis=prompt("enter numbr");
+let area;
+let circumfrance;
+area=2*3.14*raduis;
+console.log("area="+""+area)
+
