e6f02a7ff0589eed7efe4e15513fef7526da4111
eatimad <eatimad.mofleh.ajf.java@tuwaiq.edu.sa>
2021-10-14 22:46:13 +0300

    main

---
+++ java.js/java.js
@@ -0,0 +1,65 @@
+
+console.log(" the first ");
+
+
+
+let job;
+job = prompt("what is your job: ");
+
+let partnerName;
+partnerName = prompt("what is your partnerName: ");
+
+let numOfChildren;
+numOfChildren = prompt("how many children do you have: ");
+
+let geoLoation;
+geoLoation = prompt("what is your  geoLocation: ");
+
+
+console.log("YOU WILL BE A  " + job + " IN " + geoLoation + " AND MARRIED TO " + partnerName + " WITH " + numOfChildren + " KIDS ");
+
+
+
+
+console.log(" --------------- ");
+
+console.log(" the second ");
+
+
+
+let birthYear;
+birthYear = prompt(" inter your birth year : ");
+
+const currentYear = 2021
+
+let theAge;
+theAge = currentYear - birthYear;
+
+console.log(" They are : " + theAge + " years old ");
+
+
+
+console.log(" --------------- ");
+
+console.log(" the third ");
+
+
+
+let radius;
+
+radius = prompt(" inter the radius :  ");
+
+
+
+
+let theArea;
+theArea = 3.14 * (radius * radius);
+
+let circumference;
+circumference = 3.14 * (2 * radius);
+
+console.log(" theArea is : " + theArea);
+console.log(" the circumference is : " + circumference);
+
+
+
