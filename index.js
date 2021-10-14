df12f38347744c88920d90f47e9123578fb2c701
ManalAlkuraya <ManalAlkuraya1@gmail.com>
2021-10-14 20:32:59 +0300

    homeworked submit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+console.log("first code output")
+
+let numchild= prompt("how many your child?");
+let name= prompt("paratner's name");
+let job= prompt("your job");
+let glocation= prompt("your location");
+
+console.log("the number of children", numchild , "partner's name",name ,"you will be a ",job,"my nationality",glocation)
+
+
+console.log("second code output")
+
+let currentYear=prompt("year");
+let birthYear=prompt("birth");
+let age = currentYear - birthYear;
+console.log(age);
+
+
+
+console.log("third code output")
+
+function fun1(raduis){
+ let circumference =3.14*2*raduis;
+let area = 3.14*raduis*raduis;
+
+console.log("The circumference is = " + circumference)
+console.log("The area is=" + area)
+
+}
+
+fun1(2)
