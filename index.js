5e0fb8c31f684792aa447dde613095c87150661c
Abdullah Alshammari <92294783+ABDULLAHALSHMARI@users.noreply.github.com>
2021-10-15 20:03:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let x = prompt("What is your job");
+let y = prompt("getlocation");
+let z = prompt("partner name");
+let n = prompt("How many kids you has");
+let birthyear = prompt("what is your birthyear");
+currentyear = 2021
+let radius =prompt("What is radius");
+
+console.log("You will in a " +x+ " in " +y+ ", Married to " +z+ " with " +n+ " kids");
+console.log("your age =",+currentyear+ - +birthyear);
+console.log( 2 * 3.14 * radius);
