59bf7a441e28c925d29256079f24a66dfb06a9d1
Meshal552 <meshl5541@gmail.com>
2021-10-15 18:33:31 +0300

    hw5

---
+++ ml.js
@@ -0,0 +1,29 @@
+let ss;
+ss =prompt("How many kids do you have ")
+console.log("number of children: "+ss);
+let s1;
+s1 =prompt("partner's name: ")
+console.log("your husband's name: "+s1);
+let s2;
+s2 =prompt("geographic  ")
+console.log("your location: "+s2);
+let s3;
+s3 =prompt("what is your job  ")
+console.log("your job: "+s3);
+let ss1 =prompt("What is the current year? ")
+let ss2 =prompt("what is the  birth year ")
+console.log("Age", ss1-ss2);
+let radius =prompt("What is radius: ");
+let cirmcumfrence = 2 * 3.14 * radius;
+console.log("space = "+cirmcumfrence);
+
+
+
+
+
+
+
+
+
+
+
