d250679e42fc642318d33cfe090047691c1cb796
NaflaSale <naflasaleh@gmail.com>
2021-10-15 12:08:03 +0300

    THIS IS HW5

---
+++ index.js
@@ -0,0 +1,18 @@
+// This is my first js code
+
+let myName= prompt("myself") ;
+
+let numberChild = prompt("Number of child:");
+prompt("Number of child:", numberChild);
+let partnerName =prompt("MY PartnName he: ");
+prompt("MY PartnName he: " , partnerName);
+let site= prompt ("location is :");
+prompt("location:" , site);
+let job = prompt ("job is");
+prompt("job is: " , job);
+
+console.log("NEMY IS: "+ myName );
+console.log(("Number of child:" +  numberChild ) ;
+console.log( "MY PartnName he: " + partnerName ) ;
+console.log("location:" + site );
+console.log("job is: " + job );
