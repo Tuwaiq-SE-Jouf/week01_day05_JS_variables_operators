321735e0d4403e2a375eb24704f8be34e20b27bc
sultanalruwaili <sultanhayyal8@gmail.com>
2021-10-15 23:30:29 +0300

    this is the js script

---
+++ indexsultan.js
@@ -0,0 +1,27 @@
+let child;
+child = prompt("how many children do you want?")
+console.log('i want ',child);
+let wife;
+wife = prompt(' wife name?')
+console.log('your wife name will be ',wife);
+let loc;
+loc = prompt( 'where do you want to live')
+console.log( 'i want to live in ',loc);
+let job; 
+job = prompt('what do you want your job to be')
+console.log('i want to be a',job);
+
+//age calculator
+
+
+let cyear = prompt ( 'what is the current year?')
+
+let byear = prompt ( 'what is the year of birth?')
+
+console.log('age',cyear-byear);
+
+// The Geometrizer
+
+let area = prompt(" enter the number ");
+let cercal =2 *3.14*area;
+console.log("The result"+ cercal);
