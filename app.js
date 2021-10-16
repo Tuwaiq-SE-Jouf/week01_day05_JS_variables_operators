e9d288b42be786a81f1b2870e40a9f0db6946887
halah03 <mud2200@gmail.com>
2021-10-16 17:43:30 +0300

    updates.js

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-let 
-child=prompt("num of children");
-let 
-parents=prompt("parents name");
-let 
-geo=prompt("your location");
-let 
-job=prompt("your jop");
-
-
-console.log("num of child" + child ("parents name") + parents ("your location") + geo ("your jop") + job);
-
-
-
-let currentYear=prompt("current year is");
-let birthYear=prompt("Birth year is");
-
-console.log("your age", currentYear-birthYear);
-
-
 
 let area=prompt("number");
-let cercal=2 * 3.14 * area;
-console.log("The area is "+cercal);
+let cercal = (2* 3.14 *radius);
+alert( "Area: " + cercal);
+let area1 = (2*radius*radius);
+let result = (2/radius);
+alert("area"+ result);
