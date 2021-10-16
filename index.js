7a45b70b3f7b3ea38a4fd6a3c37e4a6aed4d8cfd
wafa-alruwaili <92299393+wafa-alruwaili@users.noreply.github.com>
2021-10-16 20:26:26 +0300

    h.w

---
+++ index.js
@@ -0,0 +1,20 @@
+let c1;
+c1=prompt("How many kids you have?");
+console.log("you will be have"+c1);
+let husband;
+husband=prompt("What is the husband?");
+console.log("and your husband"+husband);
+let l1;
+locatio=prompt("Where is the location?");
+console.log("and your location"+l1);
+let job;
+job=prompt("What is the job title?");
+console.log("and your job is"+job);
+
+let h1=prompt("year");
+let h2=prompt("Birth");
+console.log(h1-h2);
+
+let t1=prompt("alt");
+let t2=t1*2*3.14;
+console.log("alt="+t2);
