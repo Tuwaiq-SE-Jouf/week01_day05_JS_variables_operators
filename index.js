d9dde8d83901b2ec4d97a646ae754c084c842382
imaha2006 <imaha2006@gmail.com>
2021-10-17 09:23:47 +0300

    maha

---
+++ index.js
@@ -0,0 +1,23 @@
+let numchild= prompt("how many your child?");
+let hasbund = prompt("paratner's name:");
+let job= prompt("your job");
+let glocation= prompt("your location");
+let yourName =prompt("Enter your name");
+
+console.log("how many your child?"+numchild , "partner's name:"
+ + hasbund , "your job"+job, "your location"+glocation ,"your name is"+ yourName);
+
+ let currentYear =prompt("year");
+let birthYear =prompt("birth");
+let age = currentYear - birthYear;
+console.log("age");
+
+
+function fun1(raduis){
+    let circumference =3.14*2*raduis;
+    let area = 3.14*raduis*raduis;
+
+    console.log("the circumference is = " + circumference)
+    console.log("the area is=" + area)
+}
+fun1();
