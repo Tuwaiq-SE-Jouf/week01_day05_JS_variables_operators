7d88197cbbdd159fc181781d328d4a65a13b5d99
gar505gar <grm_505@hotmail.com>
2021-10-17 08:54:24 +0300

    update

---
+++ m.js
@@ -0,0 +1,4 @@
+let birth =prompt("enter the birth year :");
+let year =prompt("enter the current year:");
+
+console.log('your age is :'+(year-birth));
---
+++ w.js
@@ -0,0 +1,6 @@
+let =prompt('enter the radius :');
+circumference=2 redius 3.14;
+
+area= redius redius 3.14;
+console.log('the circumerence is :'+circumfarence);
+console.log('hte area is:'+ area);
