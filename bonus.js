6f9db42c991bcca44908ab27e2a4d473463cd53b
amal wash <8amal.8ll@gmail.com>
2021-10-19 05:20:50 +0300

    hw bonus

---
+++ bonus.js
@@ -0,0 +1,43 @@
+
+function radiuss(){
+
+let radius = prompt(" ENTER THE RADIUS");
+let circumference = (2 * radius* radius);
+ area = radius* radius * 3.14;
+ console.log('the result of circumference '+circumFerence);
+ console.log('the result of area ' + area);
+
+
+radiuss();
+}
+//
+function Ask(){
+    let name;
+    name =prompt("What is your first and last name?");
+    
+        let child;
+        child = prompt("How many kids do you have ?");
+    
+    let wife;
+    wife = prompt("What is the name of your wife?");
+    
+    let location;
+     location = prompt('What is your location?');
+     
+     let jop;
+     jop = prompt('what is your job');
+     
+     ;console.log('your name is '+ name , ' you have children ' + child , 'name wife is '+ wife , 'Your job is ' + jop, 'your location is '+ location);
+    
+    }
+    Ask();
+
+    //
+    
+    function birthyearr(){
+        let birthyear = prompt("Enter your year of birth");
+        let currentyear = prompt("Enter the current year");
+        
+        console.log("Your Age is " + ( currentyear -birthyear ));
+        }
+        birthyearr()
