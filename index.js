7347a19368cb6f0d58b071fa3963f346baa637af
asmaalnaser <asmahn1996@gmail.com>
2021-11-04 13:53:21 +0300

    hw

---
+++ index.js
@@ -0,0 +1,22 @@
+let a;
+a =prompt("How many kids do you have ")
+
+let b;
+b =prompt("partner's name: ")
+
+let c;
+c =prompt("geographic:  ")
+
+let d;
+d =prompt("what is your job  ")
+
+let year =prompt("What is the current year? ")
+let year1 =prompt("what is the  birth year ")
+
+console.log("number of children: "+a+".  "+"  your husband's name: "+b
++".  "+" your location: "+c+".  "+"your job: "+d+".  "+"  Age: ", year-year1);
+
+let radius =prompt("What is radius: ");
+
+let cirmcumfrence = 2 * 3.14 * radius;
+console.log("space = "+cirmcumfrence);
